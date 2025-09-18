ced76079a38b397883f9dedf6ec1388e9becb0d2
lainus quisbert <lainus.quisbert@gmail.com>
2025-09-17 23:10:41 -0400

    array con los nombres

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+//capturar el valor del campo amigo
+let nuevoAmigo = document.getElementById('amigo').value;
+
+//validamos que no sea un espacio vacio
+if(nuevoAmigo==""){
+    alert("por favor ingrese un nombre");
+    return;
+}
+
+//agregamos a la matriz
+amigos.push(nuevoAmigo);
+ alert("Agregado: " + nuevoAmigo);
+
+//limpiamos la caja de texto
+document.getElementById('amigo').value = ""; 
+
+
+}
